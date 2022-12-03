29fd67e4e63cbe7a683dd51ad6ceb20e56bc77ac
batya1871 <79578044+batya1871@users.noreply.github.com>
2022-12-03 20:01:25 +0300

    Исправил 2-е задание в 5-ой лабе

--- lab_5/reg.js
+++ lab_5/reg.js
@@ -1,66 +1,127 @@
-let userForm = document.getElementById("form-user");
-const constFormHeight = userForm.offsetHeight;
+class ValidationData{
+  constructor(span, img, errText, successText){
+    this.span = span;
+    this.img = img;
+    this.errText = errText;
+    this.successText = successText;
+  }
+  set_err(){
+    this.img.src="img/cross.PNG";
+    this.span.innerHTML = this.errText;
+    this.span.style.color = "red";
+  }
+  set_success(){
+    this.img.src="img/galka.PNG";
+    this.span.innerHTML = this.successText;
+    this.span.style.color = "rgb(90,174,107)";
+  }
+}
 
 
 let checkbox = document.getElementById("user-checkbox");
 let btn = document.getElementById("btn")
 
+
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     btn.removeAttribute('disabled');
+    btn.style = "user-post";
   } else {
     btn.setAttribute('disabled', true);
+    btn.style.background = "gray";
   }
 });
 
 let username = document.getElementById("user-name");
 let secondname = document.getElementById("user-secondname");
-let login = document.getElementById("user-login");
-let email = document.getElementById("user-email")
-let pass = document.getElementById("user-pass");
-let pass2 = document.getElementById("user-pass-2");
 
-let loginErr = document.getElementById("err-login");
-let passErr = document.getElementById("err-pass");
-let pass2Err = document.getElementById("err-pass2");
+let login = document.getElementById("user-login");
+var login_val = new Map();
+login_val.set("symbols", new ValidationData(document.getElementById("val-log-symbols"), document.getElementById("val-log-symbols-img"),
+"Используются недопустимые символы!", "Только буквы, цифры и символы @.+-_"));
+login_val.set("len5", new ValidationData(document.getElementById("val-log-5"), document.getElementById("val-log-5-img"),
+"Логин должен быть не менее 5-ти символов!", "Не более 5 символов"));
+login_val.set("len150", new ValidationData(document.getElementById("val-log-150"), document.getElementById("val-log-150-img"),
+"Логин должен быть меньше 150-ти символов!", "Не более 150 символов"));
+login_val.set("log-pass", new ValidationData(document.getElementById("val-log-pass"), document.getElementById("val-log-pass-img"),
+"Логин должен отличаться от пароля!", "Не cовпадает с паролем"));
+
+let email = document.getElementById("user-email");
+
+let pass = document.getElementById("user-pass");
+var pass_val = new Map();
+pass_val.set("len8", new ValidationData(document.getElementById("val-pass-8"),document.getElementById("val-pass-8-img"),
+"Пароль должен быть не менее 8-ми символов!", "Не менее 8-ми символов"));
+pass_val.set("len25", new ValidationData(document.getElementById("val-pass-25"),document.getElementById("val-pass-25-img"),
+"Пароль должен быть меньше 25-ти символов!", "Не превышает 25 символов"));
+pass_val.set("symbols", new ValidationData(document.getElementById("val-pass-symbols"),document.getElementById("val-pass-symbols-img"),
+"Пароль должен включать минимум один не цифровой символ!", "Не состоит только из цифр"));
+pass_val.set("pass-log", new ValidationData(document.getElementById("val-pass-pass2"), document.getElementById("val-pass-pass2-img"),
+"Логин должен отличаться от логина!", "Не cовпадает с логином"));
+
+let pass2 = document.getElementById("user-pass-2");
+var pass2_val = new Map();
+pass2_val.set("len8", new ValidationData(document.getElementById("val-pass2-8"),document.getElementById("val-pass2-8-img"),
+"Пароль должен быть не менее 8-ми символов!", "Не менее 8-ми символов"));
+pass2_val.set("len25", new ValidationData(document.getElementById("val-pass2-25"),document.getElementById("val-pass2-25-img"),
+"Пароль должен быть меньше 25-ти символов!", "Не превышает 25 символов"));
+pass2_val.set("symbols", new ValidationData(document.getElementById("val-pass2-symbols"),document.getElementById("val-pass2-symbols-img"),
+"Пароль должен включать минимум один не цифровой символ!", "Не состоит только из цифр"));
+pass2_val.set("pass2-pass", new ValidationData(document.getElementById("val-pass2-pass"), document.getElementById("val-pass2-pass-img"),
+"Пароли не совпадают!", "Пароли совпадают"));
+
+function getGreen(map){
+  for (var [key, value] of map) {
+    value.set_success();
+  }
+}
+
+
 
 username.addEventListener('input', function(){
   if (!/^([A-я]|[A-z]|\s|-)*$/.test(username.value)) {
       username.style.borderColor = "red";
+      username.style.color = "red";
   }
   else {
     username.style.borderColor = "";
+    username.style.color = "black";
   }
 })
 
 secondname.addEventListener('input', function(){
   if (!/^([A-я]|[A-z]|\s|-)*$/.test(secondname.value)) {
     secondname.style.borderColor = "red";
+    secondname.style.color = "red";
   }
   else {
     secondname.style.borderColor = "";
+    secondname.style.color = "black";
   }
 })
 
 login.addEventListener('input', function(){
-  let err = "";
+  getGreen(login_val);
+  pass_val.get("pass-log").set_success();
   if (!/^([A-я]|[A-z]|\d|@|\.|\+|-|_)*$/.test(login.value)) {
     login.style.borderColor = "red";
-    err += "Доступны только буквы, цифры и символы @.+-_!<br>"
+    login.style.color = "red";
+    login_val.get("symbols").set_err();
   }
   else {
     login.style.borderColor = "";
+    login.style.color = "black";
   }
   if ((login.value.length < 5) && (login.value != "")){
-    err += "Логин должен содержать больше 5-ти символов!<br>"
+    login_val.get("len5").set_err();
   }
   if (login.value.length > 150){
-    err += "Логин должен содержать меньше 150-ти символов!<br>"
+    login_val.get("len150").set_err();
   }
   if ((login.value == pass.value) && (login.value != "")) {
-    err += "Логин не должен совпадать с паролем!<br>"
+    login_val.get("log-pass").set_err();
+    pass_val.get("pass-log").set_err();
   }
-  loginErr.innerHTML = err;
   
   
 })
@@ -68,58 +129,63 @@ login.addEventListener('input', function(){
 email.addEventListener('input', function(){
   if ((!/^([A-z]|\d|_|-)+@(mail|gmail|yandex)\.(ru|net)$/.test(email.value)) && (email.value != "")) {
     email.style.borderColor = "red";
+    email.style.color = "red";
   }
   else {
     email.style.borderColor = "";
+    email.style.color = "black";
   }
 })
 
 pass.addEventListener('input', function(){
-  let err = "";
+  getGreen(pass_val);
+  login_val.get("log-pass").set_success();
   if ((!/^.*\D.*$/.test(pass.value))&& (pass.value != "")) {
     pass.style.borderColor = "red";
-    err += "Пароль должен содержать минимум один нецифровой символ!<br>"
+    pass.style.color = "red";
+    pass_val.get("symbols").set_err();
   }
   else {
     pass.style.borderColor = "";
+    pass.style.color = "black";
   }
 
   if ((pass.value.length < 8) && (pass.value != "")){
-    err += "Пароль должен содержать больше 8-ти символов!<br>"
+    pass_val.get("len8").set_err();
   }
   if (pass.value.length > 25){
-    err += "Логин должен содержать меньше 25-ти символов!<br>"
+    pass_val.get("len25").set_err();
   }
   if ((login.value == pass.value) && (login.value != "")) {
-    err += "Логин не должен совпадать с паролем!<br>"
+    pass_val.get("pass-log").set_err();
+    login_val.get("log-pass").set_err();
   }
-  passErr.innerHTML = err;
-  
 })
 
 
 
 pass2.addEventListener('input', function(){
-  let err = ""
+  getGreen(pass2_val);
   if ((!/^.*\D.*$/.test(pass2.value))&& (pass2.value != "")) {
     pass2.style.borderColor = "red";
-    err += "Пароль должен содержать минимум один нецифровой символ!<br>"
+    pass2.style.color = "red";
+    pass2_val.get("symbols").set_err();
   }
   else {
     pass2.style.borderColor = "";
+    pass2.style.color = "black";
   }
 
   
   if ((pass2.value.length < 8) && (pass2.value != "")){
-    err += "Пароль должен содержать больше 8-ти символов!<br>"
+    pass2_val.get("len8").set_err();
   }
   if (pass2.value.length > 25){
-    err += "Пароль должен содержать меньше 25-ти символов!<br>"
+    pass2_val.get("len25").set_err();
   }
   if ((pass2.value != pass.value) && (pass2.value != "")) {
-    err += "Пароли должны совпадать!<br>"
+    pass2_val.get("pass2-pass").set_err();
   }
-  pass2Err.innerHTML = err;
 })
 
 function validateMyForm(){
